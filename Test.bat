60ae4d13009613b3d46156b4aca01bf5122b259d
Protik Banerji <protik09@users.noreply.github.com>
2017-02-01 20:36:16 +0530

    Add files via upload

---
+++ Test.bat
@@ -0,0 +1,149 @@
+@echo Off
+TITLE Install Dev Environment (wget, Python and python libraries)
+echo.
+@echo :::: Install Dev Environment (wget, Python, Arduino and python libraries) ::::
+echo.
+
+:::::: Initialise global variables
+
+:::: Change links to most recent ones manually
+set wgetlink=https://eternallybored.org/misc/wget/current/wget64.exe
+set pythlink=https://www.python.org/ftp/python/2.7.13/python-2.7.13.amd64.msi
+set llvmlink=http://releases.llvm.org/3.9.1/LLVM-3.9.1-win64.exe
+set ardlink=LINK
+
+:::: Change where programs are installed on your comp already or will be installed
+set wgetdir=C:\wget64.exe
+set pythondir=C:\Python27\python.exe
+set clangdir=C:\Program Files\LLVM\bin\clang.exe
+set arddir=https://www.arduino.cc/download_handler.php
+
+:::: Error Messages
+set exit=Program is now exiting...
+set w64err=ERROR: wget64.exe not found in %wgetdir%.
+set pytherr=ERROR: python.exe not found in %pythondir%
+
+:::: Commmand Shortcuts
+set download=C:\wget64.exe -nc --directory-prefix=Downloads
+
+:::::: Check what programs exist
+
+:: The &echo. at the end adds the newline to the next line so you get a skipped line
+@echo Checking for existing programs...&echo.
+if exist %wgetdir% (@echo SUCCESS: wget64.exe exists in C&echo.) else (call:Downwget64)
+::Wait for user input to ensure that everything happened successfully
+PAUSE
+
+if exist %pythondir% (@echo SUCCESS: Python exists on this system.) else (
+	call:DownPyth27)
+
+
+:::: Install your standard python libs via pip
+rem This is the python pip install section
+rem <# Please wait 5 seconds..... #>
+timeout /t 5
+python -m pip install --upgrade pip
+python -m pip install --upgrade setuptools
+python -m pip install --upgrade python-build
+python -m pip install --upgrade matplotlib
+python -m pip install --upgrade numpy
+python -m pip install --upgrade beautifulsoup4
+python -m pip install --upgrade pyserial
+python -m pip install --upgrade pyaudio
+python -m pip install --upgrade pyusb
+python -m pip install --upgrade jupyter
+python -m pip install --upgrade pytools
+rem <#Remember to run these commands in the atom PS shell#>
+timeout /t 5
+
+@echo "Installing Python linter for Atom...."
+timeout /t 5
+python -m pip install --upgrade flake8
+python -m pip install --upgrade flake8-docstrings
+rem <#apm install linter-flake8 #>
+rem <#Remember to run these above in the atom PS shell#>
+
+@echo Dowloading required distributions via wget cmdlet
+if exist source\NUL @echo Folder Already Exists. Downloading...
+
+rem https://eternallybored.org/misc/wget/
+
+echo Copying files to Python Scripts folder.....
+timeout /t 5
+
+exit /B
+
+:Downwget64
+echo.%w64err%&echo.
+@echo Do you want to install wget64.exe? You WILL need it.
+set /P wgetdown="Do you wish to install wget? (Y/N): "
+:: The /I switch makes the input letter case-insensitive
+if /I %wgetdown%==n echo.%exit%&call:ExitBatch
+:: if %wgetdown%=="N" (call:ExitBatch&goto:eof)
+if /I %wgetdown%==y call:installncheck_wget
+:: if %wgetdown%=="Y" (call:installncheck_wget)
+goto:eof
+
+:installncheck_wget
+echo.
+@echo Function running: installncheck_wget
+echo.
+@echo Transfer wget64 to C:\ otherwise this batch will terminate.
+:: Download wget64.exe via web browser
+start %wgetlink%
+echo.
+PAUSE
+:: Check to make sure wget64 is indeed there in C drive otherwise exit
+if exist C:\wget64.exe (@echo SUCCESS: wget64.exe exists. Continuing...&echo.) else (
+	echo.&echo.%w64err% %exit%&call:ExitBatch)
+goto:eof
+
+:DownPyth27
+echo.
+@echo %pytherr%
+set /P pythondown="Do you wish to install Python27? (Y/N): "
+if /I %pythondown%==n call:ExitBatch
+:: if %pythondown%=="N" (call:ExitBatch&goto:eof)
+if /I %pythondown%==y call:installncheck_python
+:: if %pythondown%=="Y" (call:installncheck_python)
+goto:eof
+
+:installncheck_python
+echo.
+@echo Function running: installncheck_python&echo.
+@echo Installing python27.....
+:: wget with no overwrite existing file -nc and download directory Downloads
+%download% %pythlink%
+rem C:\wget64.exe -nc --directory-prefix=Downloads %pythlink%
+cd Downloads
+msiexec /i python-2.7.13.amd64.msi TARGETDIR=C:\Python27 ALLUSERS=1
+cd..
+:: Check to make sure python is properly installed in c:\Python27
+if exist %pythondir% (@echo SUCCESS: Python was succesfully installed.) else (
+	echo.&call:DownPyth27)
+echo.&pause&goto:eof
+
+
+
+
+
+
+
+:: CLEAN BATCH EXIT SECTION from https://stackoverflow.com/questions/3227796/exit-batch-script-from-inside-a-function/25474648#25474648
+@echo off
+:ExitBatch - Cleanly exit batch processing, regardless how many CALLs
+if not exist "%temp%\ExitBatchYes.txt" call :buildYes
+call :CtrlC <"%temp%\ExitBatchYes.txt" 1>nul 2>&1
+:CtrlC
+cmd /c exit -1073741510
+
+:buildYes - Establish a Yes file for the language used by the OS
+pushd "%temp%"
+set "yes="
+copy nul ExitBatchYes.txt >nul
+for /f "delims=(/ tokens=2" %%Y in (
+  '"copy /-y nul ExitBatchYes.txt <nul"'
+) do if not defined yes set "yes=%%Y"
+echo %yes%>ExitBatchYes.txt
+popd
+exit /b
