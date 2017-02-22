4d61dc55d9963966c87e7119c89934fc081bd6b0
Protik Banerji <protik09@gmail.com>
2017-02-23 01:33:40 +0530

    Siwtch to tronscript type batch script
    Use tronscript to learn advanced bath file design

---
+++ Main.bat
@@ -0,0 +1,4 @@
+:: Purpose:       Set up various dev environements with ease on a Windows PC. All Windows versions XP and up are supported
+::                  Protik Banerji:  "Guy who got annoyed having to setup his dev environment on muliple computers."
+:: Requirements:  1. Administrator access
+::
